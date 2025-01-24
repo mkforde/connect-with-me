89007decfacdd61087736fc7bf2fc4b69c32dffb
Michael Forde <michael.kenneth.forde@gmail.com>
2025-01-23 22:53:49 -0500

    Make links clickable

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ document.querySelector(".profile").addEventListener("click", () => {
   location.href = "https://www.linkedin.com/in/michaelkforde/";
 });
 
-document.getElementById("email").addEventListener("click", function () {
-  let name = "michael.kenneth.forde";
-  let domain = "gmail.com";
-  let email = name + "@" + domain;
-  window.location.href = "mailto:" + email;
+document.querySelector(".container").addEventListener("click", (e) => {
+  // Check if the clicked element has the class 'social' and the expected id
+  if (e.target.closest(".social")) {
+    const target = e.target.closest(".social"); // Find the closest social div
+    const id = target.id; // Get the ID of the clicked social div
+
+    switch (id) {
+      case "blog":
+        window.location = "https://blog.michaelforde.com";
+        break;
+      case "github":
+        window.location = "https://github.com/mkforde";
+        break;
+      case "email":
+        let name = "michael.kenneth.forde";
+        let domain = "gmail.com";
+        let email = name + "@" + domain;
+        window.location = "mailto:" + email;
+        break;
+      case "insta":
+        window.location = "https://www.instagram.com/_michaelforde/";
+        break;
+      default:
+        break;
+    }
+  }
 });
